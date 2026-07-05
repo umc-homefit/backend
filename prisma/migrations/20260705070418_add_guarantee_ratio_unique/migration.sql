00074a91b3d2edf268437a8be3adf3d977a48161
정지훈 <sunzerok83@gmail.com>
2026-07-05 22:32:26 +0900

    refactor: 피드백 반영

--- prisma/migrations/20260705070418_add_guarantee_ratio_unique/migration.sql
+++ prisma/migrations/20260705070418_add_guarantee_ratio_unique/migration.sql
@@ -1,7 +1,7 @@
 /*
   Warnings:
 
-  - A unique constraint covering the columns `[provider_name,guarantee_ratio]` on the table `loan_products` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[provider_name,product_name,guarantee_ratio]` on the table `loan_products` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- AlterTable
@@ -11,4 +11,4 @@ ALTER COLUMN "require_no_house" DROP DEFAULT,
 ALTER COLUMN "official_url" DROP NOT NULL;
 
 -- CreateIndex
-CREATE UNIQUE INDEX "UK_LOAN_PRODUCTS_PROVIDER_GUARANTEE" ON "loan_products"("provider_name", "guarantee_ratio");
+CREATE UNIQUE INDEX "UK_LOAN_PRODUCTS_PROVIDER_PRODUCT_GUARANTEE" ON "loan_products"("provider_name", "product_name", "guarantee_ratio");
