ddb48751ab763991af75304ce166a65c81ff5d2c
정지훈 <sunzerok83@gmail.com>
2026-08-12 01:50:40 +0900

    [Chore] 미사용 loanRatioPercent 컬럼 제거

---
+++ prisma/migrations/20260811164658_remove_unused_loan_ratio_percent/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `loan_ratio_percent` on the `loan_products` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "loan_products" DROP COLUMN "loan_ratio_percent";
